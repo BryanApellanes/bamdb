77dbcb01e164592a205e55c7617edf4c4413d436
Bryan Apellanes <bryan@threeheadz.com>
2023-01-20 20:11:29 -0600

    humpty dumpty back together

--- bamdb/Program.cs
+++ bamdb/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Bam.Net.Logging;
+using Bam.Net.Testing;
+using System;
+using Bam.Net.CommandLine;
+using Bam.Shell;
+using Bam.Shell.CodeGen;
+using Bam.Shell.Data;
+
+namespace Bam.Net.Application
+{
+    [Serializable]
+    class Program : CommandLineTool
+    {
+        static void Main(string[] args)
+        {
+            Log.Default = Workspace.Current.CreateLogger<TextFileLogger>();
+            TryWritePid();
+
+            IsolateMethodCalls = false;
+            
+            BamEnvironmentVariables.SetBamVariable("ApplicationName", "bamdb");
+            
+            ArgumentAdder.AddArguments(args);
+            
+            ExecuteMainOrInteractive(args);
+        }
+    }
+}
